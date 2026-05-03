f4d525721f0f257617d214cbc39a45c939c9489d
Bruno Abude Cardoso <brunoabude@outlook.com>
2026-05-03 18:15:34 -0300

    Add: two-integer-sum-ii - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-8.go
@@ -0,0 +1,23 @@
+func twoSum(numbers []int, target int) []int {
+	if len(numbers) == 2 {
+		return []int{1, 2}
+	}
+
+	l, r := 0, len(numbers)-1
+
+	for range numbers {
+		if numbers[l]+numbers[r] > target {
+			r--
+		}
+
+		if numbers[l]+numbers[r] < target {
+			l++
+		}
+
+		if numbers[l]+numbers[r] == target {
+			return []int{1 + l, 1 + r}
+		}
+	}
+
+	return []int{-1, -1}
+}
