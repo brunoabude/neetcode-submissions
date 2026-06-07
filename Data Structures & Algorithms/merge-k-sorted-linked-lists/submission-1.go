3202c6500b7c43d9205e9cd8f943eeb030a3ee54
Bruno Abude Cardoso <brunoabude@outlook.com>
2026-06-07 19:51:29 -0300

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.go
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+
+func mergeK(lists []*ListNode, l, r int) *ListNode {
+	if l > r {
+		return nil
+	}
+
+	if r >= len(lists) {
+		return nil
+	}
+
+	if l == r {
+		return lists[l]
+	}
+
+	m := (l+r) / 2
+
+	l1 := mergeK(lists , l, m)
+	l2 := mergeK(lists , m+1, r)
+
+	return merge(l1, l2)
+}
+
+func merge(l, r *ListNode) *ListNode {
+	dummy := &ListNode{}
+	curr := dummy
+
+	for l != nil && r != nil {
+		if l.Val < r.Val {
+			curr.Next = l
+			curr = curr.Next
+			l = l.Next
+		} else {
+			curr.Next = r
+			curr = curr.Next
+			r = r.Next			
+		}
+	}
+
+	for l != nil {
+		curr.Next = l
+		curr = curr.Next
+		l = l.Next
+	}
+
+	for r != nil {
+		curr.Next = r
+		curr = curr.Next
+		r = r.Next
+	}
+
+	return dummy.Next
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	return mergeK(lists, 0, len(lists)-1)
+}
