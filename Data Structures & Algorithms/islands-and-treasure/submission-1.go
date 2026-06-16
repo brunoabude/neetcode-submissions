71553020868a9c29cebed028da455d6e4f5c2e7b
Bruno Abude Cardoso <brunoabude@outlook.com>
2026-06-15 23:14:39 -0300

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.go
@@ -0,0 +1,44 @@
+const inf int = 2147483647
+var (
+	left        [2]int   = [2]int{0, -1}
+	right       [2]int   = [2]int{0, 1}
+	up          [2]int   = [2]int{-1, 0}
+	down        [2]int   = [2]int{1, 0}
+	directions  [][2]int = [][2]int{up, down, left, right}
+)
+
+func islandsAndTreasure(grid [][]int) {
+	m, n := len(grid), len(grid[0])
+	queue := [][2]int{}
+
+	for i := range m {
+		for j := range n {
+			if grid[i][j] == 0 {
+				queue = append(queue, [2]int{i, j})
+			}
+		}
+	}
+
+
+	for len(queue) > 0 {
+		queueLength := len(queue)
+
+		for cellIdx := range queueLength {
+			cell := queue[cellIdx]
+			i, j := cell[0], cell[1]
+			
+			level := grid[i][j] + 1
+
+			for _, dir := range directions {
+				if i+dir[0] < 0 || i+dir[0] >= m || j+dir[1] < 0 || j+dir[1] >= n || grid[i+dir[0]][j+dir[1]] != inf {
+					continue	
+				}
+
+				grid[i+dir[0]][j+dir[1]] = level
+				queue = append(queue, [2]int{i+dir[0], j+dir[1]})
+			}
+		}
+
+		queue = queue[queueLength:]
+	}
+}
